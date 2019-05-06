4e1d41b4e55cdaecfbb73ae3d903609408b02ec2
Alane <bibang007@gmail.com>
2019-05-06 18:11:21 -0400

    define state

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { withRouter } from 'react-router';
 import decode from 'jwt-decode';
-import Register from './components/Register'
+
 import Login from './components/Login'
+import Register from './components/Register'
 
 import {
   loginUser,
   registerUser,
 } from './services/api-helper';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+      postForm: {
+      name: "",
+      price: "",
+      category: "",
+      content:"",
+      image: ""
+      },
+      currentUser: null,
+      authFormData: {
+        username: "",
+        email: "",
+        password_digest: ""
+      }
+    };
 
 
 function App() {
@@ -18,4 +41,4 @@ function App() {
   );
 }
 
-export default App;
+export default withRouter(App);
