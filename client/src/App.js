d4d24965c570002b79ee52c347d4c6f66ca054cb
Alane <bibang007@gmail.com>
2019-05-06 18:10:23 -0400

    define state

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,12 @@ import decode from 'jwt-decode';
 import Register from './components/Register'
 import Login from './components/Login'
 
+import {
+  loginUser,
+  registerUser,
+} from './services/api-helper';
+
+
 function App() {
   return (
     <div className="App">
---
+++ client/src/components/services/api-helper.js
@@ -0,0 +1,29 @@
+const baseUrl = "http://localhost:3000";
+
+export const loginUser = (loginData) => {
+    const opts = {
+      method: 'POST',
+      body: JSON.stringify({ auth: loginData }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    return fetch(`${baseUrl}/user_token`, opts)
+      .then(resp => resp.json())
+      .catch(e => e)
+  }
+  
+  export const registerUser = (registerData) => {
+    const opts = {
+      method: 'POST',
+      body: JSON.stringify({ user: registerData }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  
+    return fetch(`${baseUrl}/users`, opts)
+      .then(resp => resp.json())
+      .catch(e => e)
+  }
+  
