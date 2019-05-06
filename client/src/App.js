fcfec2ef8bc75ffcb91ca425f05e3a442e5148e4
Alane <bibang007@gmail.com>
2019-05-06 15:53:34 -0400

    created react client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import decode from 'jwt-decode';
+import Register from './components/Register'
+import Login from './components/Login'
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
