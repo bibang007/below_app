d4a7c5974257130c7ce013327c26321b44d2a27c
Alane <bibang007@gmail.com>
2019-05-07 10:42:19 -0400

    update api helper page

--- client/src/components/services/api-helper.js
+++ client/src/components/services/api-helper.js
@@ -1,29 +1,70 @@
-const baseUrl = "http://localhost:3000";
+const baseUrl = 'http://localhost:3000'
 
 export const loginUser = (loginData) => {
-    const opts = {
-      method: 'POST',
-      body: JSON.stringify({ auth: loginData }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
+  const opts = {
+    method: 'POST',
+    body: JSON.stringify(loginData),
+    headers: {
+      'Content-Type': 'application/json'
     }
-    return fetch(`${baseUrl}/user_token`, opts)
-      .then(resp => resp.json())
-      .catch(e => e)
-  }
-  
+  };
+
+  return fetch(`${baseUrl}/auth/login`, opts)
+    .then(resp => resp.json())
+}
+
 export const registerUser = (registerData) => {
-    const opts = {
-      method: 'POST',
-      body: JSON.stringify({ user: registerData }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
+  const opts = {
+    method: 'POST',
+    body: JSON.stringify({ user: registerData }),
+    headers: {
+      'Content-Type': 'application/json'
     }
-  
-    return fetch(`${baseUrl}/users`, opts)
-      .then(resp => resp.json())
-      .catch(e => e)
+  };
+
+  return fetch(`${baseUrl}/users/`, opts)
+    .then(resp => resp.json())
+}
+
+const createPost = (data) => {
+  const opts = {
+    method: 'POST',
+    body: JSON.stringify({ post: data }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
   }
-  
+  return fetch(`${baseUrl}/posts`, opts)
+    .then(resp => resp.json())
+}
+
+const readAllPosts = () => {
+  return fetch(`${baseUrl}/posts`)
+    .then(resp => resp.json())
+}
+
+const updatePost = (id, data) => {
+  const opts = {
+    method: 'PUT',
+    body: JSON.stringify({ post: data }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  return fetch(`${baseUrl}/posts/${id}`, opts)
+    .then(resp => resp.json())
+}
+
+const destroyPost = (id) => {
+  const opts = {
+    method: 'DELETE'
+  }
+  return fetch(`${baseUrl}/posts/${id}`, opts)
+}
+
+export {
+  createPost,
+  readAllPosts,
+  updatePost,
+  destroyPost
+}
